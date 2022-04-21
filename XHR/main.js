aecbc29b16197d527d2efa91b057b5f3ae159486
JHodgkins <jhodgkins78@gmail.com>
2022-04-21 00:31:33 +0000

    code: retrieved data to the console

--- XHR/main.js
+++ XHR/main.js
@@ -1,11 +1,18 @@
 // listener
 var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function() {
-    if(this.readyState == 4 && this.status == 200) {
-        document.getElementById('data').innerHTML = this.responseText;
-    }
-};
+var data;
 
 // open connection
 xhr.open('get', 'https://ci-swapi.herokuapp.com/api/');
 xhr.send();
+
+xhr.onreadystatechange = function() {
+    if(this.readyState == 4 && this.status == 200) {
+        document.getElementById('data').innerHTML = this.responseText;
+        data = this.responseText;
+        console.log(data);
+        // console.log(typeof(this.responseText)); checks type of data returned
+        // console.log(typeof(JSON.parse(this.responseText))); parse data into a object
+        // console.log(JSON.parse(this.responseText)); log the json to console
+    }
+};
