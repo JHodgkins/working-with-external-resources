3f9f9baf4aac8106ddc6eb023e01f35a593a3dc1
JHodgkins <jhodgkins78@gmail.com>
2022-04-21 16:03:18 +0000

    code: updated examples with notes

--- XHR/main.js
+++ XHR/main.js
@@ -7,22 +7,24 @@ xhr.open('get', 'https://ci-swapi.herokuapp.com/api/');
 xhr.send();
 
 // set returned data to object and assign variable
-function setData(jsonData) {
-    data = jsonData;
-    console.log(data);
-}
+// function setData(jsonData) {
+//     data = jsonData;
+//     console.log(data);
+// }
 
 xhr.onreadystatechange = function() {
+    // console.log(this.readyState); // see what the ready state is doing
     if(this.readyState == 4 && this.status == 200) {
         document.getElementById('data').innerHTML = this.responseText;
-        setData(JSON.parse(this.responseText)); 
+        data = JSON.parse(this.responseText);
+        //setData(JSON.parse(this.responseText)); 
         //console.log(data);
         // console.log(typeof(this.responseText)); checks type of data returned
         // console.log(typeof(JSON.parse(this.responseText))); parse data into a object
         // console.log(JSON.parse(this.responseText)); log the json to console
     }
 };
-
-/**
- * Notes: while the json data has moved out of xhr into a setData variable, the problem of data is not available anywhere else remains, timeouts may solve this.
- */
+// adding a timeout now allows the data to be ready when console.log is called, so json.parse can be added back to the original call.
+setTimeout(function(){
+    console.log(data);
+}, 500);
